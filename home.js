09017675ba8a70e547003ff78464ad15a9c63e6a
artur zainotti <86525338+artbwz@users.noreply.github.com>
2026-04-13 21:22:59 +0000

    fix: Ajuste redirect home

--- home.js
+++ home.js
@@ -36,3 +36,16 @@ if (productCards.length > 0) {
     });
   });
 }
+
+const categoryLinks = document.querySelectorAll(".categorias-dropdown a");
+
+categoryLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const category = link.dataset.category;
+    localStorage.setItem("selectedCategory", category);
+
+    window.location.href = "/frontend/pages/home/index.html";
+  });
+});
