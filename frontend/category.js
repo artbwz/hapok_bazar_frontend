6b03a8c407e51634656e58985902473cfbf244f1
Carlos Barreto <129400612+dev-cerb@users.noreply.github.com>
2026-04-13 22:00:27 +0000

    `fixx:` route

--- frontend/category.js
+++ frontend/category.js
@@ -7,6 +7,6 @@ categoryLinks.forEach((link) => {
     const category = link.dataset.category;
     localStorage.setItem("selectedCategory", category);
 
-    window.location.href = "/frontend/pages/home/index.html";
+    window.location.href = "../frontend/pages/home/index.html";
   });
 });
