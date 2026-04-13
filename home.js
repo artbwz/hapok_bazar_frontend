4dea4f24f4ff1b27d214740b059a66e67a5d78f5
Carlos Barreto <129400612+dev-cerb@users.noreply.github.com>
2026-04-13 21:51:41 +0000

    `fixx`:: links

--- home.js
+++ home.js
@@ -12,7 +12,8 @@ categoryLink.forEach((link) => {
     const category = link.dataset.category;
     localStorage.setItem("selectedCategory", category);
 
-    window.location.href = "/frontend/pages/home/index.html";
+    window.location.href =
+      "../hapok_bazar_frontend/frontend/pages/home/index.html";
   });
 });
 
